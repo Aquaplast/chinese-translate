54e8202ada2339130cf1f0f3101935aaef402536
Тарасов Дмитрий Игоревич <06782328@sigma.sbrf.ru>
2018-02-19 14:50:50 +0300

    yoyo

--- parses.php
+++ parses.php
@@ -5,18 +5,44 @@ require_once("parseChinese.php");
 $text = file_get_contents("*.xhtml");
 
 $parsed = parseChinese($text, "mandarin_words.txt");
-
-//rewrite
-$file = "result.txt";
-//Проверить будет ли тут перезапись
-fopen($file, "w");
-$result = file_get_contents($file);
-
 $count = count($parsed);
-for ($i=0; $i < $count; $i++) {    
-    $result .= $parsed[$i] . "\r\n";
+
+$rez = "";
+
+for ($j=0; $j < $count; $j++) {    
+
+	$word = $parsed[$j];
+	$rez .= $word . "\r\n";
+	$urlencoded_word = urlencode($word);
+	
+	$pinyin_url = "https://glosbe.com/transliteration/api?from=Han&dest=Latin&text=".$urlencoded_word."&format=json";
+	//запросы к API
+
+	$response_pinyin = "";
+	$json_pinyin = json_decode($response_pinyin);
+	if ($json_pinyin["result"] == "ok") {
+		$rez .= $json_pinyin["text"] . "\r\n";
+	}
+	
+	$translate_url = "https://glosbe.com/gapi/translate?from=zho&dest=rus&format=json&phrase=".$urlencoded_word."&pretty=true";	
+	//запросы к API
+
+	$response_translate = "";
+	$json_translate = json_decode($response_translate);
+	if ($json_translate["result"] == "ok") {
+		$translation_count = count($json_translate["tuc"]);
+		for ($i=0; $i < min($translation_count, 3); $i++) {
+			$rez .= $json_translate["tuc"][$i]["phrase"]["text"] . "\r\n";
+		}	
+	}
+
+	$rez .= "\r\n";
+
 }
 
-file_put_contents($file, $result);
+//Записть результата в файл (промежуточный рез.)
+$file = "result.txt";
+fopen($file, "w");
+file_put_contents($file, $rez);
 
 ?>
